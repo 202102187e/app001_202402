6df4a6dd2b143d4226304ffb3e3f2060bb26a7d3
202102187e <202102187e@utea.edu.pe>
2024-12-11 07:51:33 -0500

    Perfil tarea

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,119 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFFffc300), //Colors.deepPurple,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 200,
-                // height: 200,
-                color: Colors.redAccent,
-                child: Image.network(
-                  "https://images.pexels.com/photos/29624729/pexels-photo-29624729.jpeg",
-                  fit: BoxFit.fill,
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue, // Fondo azul
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 60,
+              backgroundImage: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmjOqXLV2SxaORyh3si2uuQJxylXwf3BfexqCW24xJpAUAvbi3_sZb9ncbHK4K2beQR0Q&usqp=CAU',
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Fiorella de Fátima Segovia',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const Text(
+              'CARIÑOSA',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+                letterSpacing: 2.5,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Card(
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.blue,
                 ),
+                title: Text('Phone'),
+                subtitle: Text('+51 969461067'),
               ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  int numero = 100;
-                  print("ElevatedButton $numero");
-                },
-                child: const Text("ElevatedButton"),
-              ),
-              ElevatedButton.icon(
-                onPressed: () {},
-                label: Text("ElevatedButton Icon"),
-                icon: Icon(
-                  Icons.access_alarm_rounded,
+            ),
+            Card(
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.blue,
                 ),
+                title: Text('Email'),
+                subtitle: Text('chatanasSegovia69@gmail.com'),
               ),
-              FloatingActionButton(
-                onPressed: () {
-                  print("Click, desde el Floating Action Button");
-                },
-                child: const Icon(
-                  Icons.plus_one,
+            ),
+            // Tarjeta para OnlyFans
+            Card(
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+              child: ListTile(
+                leading: Image.network(
+                  'https://1000marcas.net/wp-content/uploads/2022/01/Icon-Onlyfans-Logo.png',
+                  width: 30.0,
+                  height: 30.0,
+                  fit: BoxFit.cover,
                 ),
+                title: const Text('OnlyFans'),
+                subtitle: const Text('OnlyFans.com/aryvilchis/c1'),
               ),
-              FilledButton(
-                onPressed: () {},
-                child: Text("FilledButton"),
-              ),
-              FilledButton.icon(
-                onPressed: () {},
-                label: Text("FilledButton Icon"),
-                icon: Icon(Icons.accessibility_new),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: Text("OutlineButton"),
-              ),
-              OutlinedButton.icon(
-                onPressed: () {},
-                label: Text("OutlineButton Icon"),
-                icon: Icon(Icons.account_box_outlined),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text("TextButton"),
-              ),
-              TextButton.icon(
-                onPressed: () {},
-                label: Text("TextButton Icon"),
-                icon: Icon(Icons.terminal),
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.facebook,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 20),
+                Icon(
+                  Icons.alternate_email,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 20),
+                Icon(
+                  Icons.camera_alt,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
